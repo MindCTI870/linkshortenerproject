72ba1b68a28335fa5cd7dd80a500a792c4d58779
cosmin.duma <cosmin.duma@mindcti.com>
2026-04-15 15:00:43 +0300

    Remove Sign In button from CTAButtons component

--- components/cta-buttons.tsx
+++ components/cta-buttons.tsx
@@ -4,20 +4,12 @@ import { useClerk } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 
 export function CTAButtons() {
-  const { openSignUp, openSignIn } = useClerk();
+  const { openSignUp } = useClerk();
 
   return (
     <div className="flex flex-col sm:flex-row gap-4 justify-center">
       <Button size="lg" onClick={() => openSignUp()} className="text-base px-8">
         Get Started Free
-      </Button>
-      <Button
-        size="lg"
-        variant="outline"
-        onClick={() => openSignIn()}
-        className="text-base px-8"
-      >
-        Sign In
       </Button>
     </div>
   );
